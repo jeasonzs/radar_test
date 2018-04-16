8c3d841821dd4842cd4d276515a9903c3df70612
jeason <123652133@qq.com>
2018-04-17 01:06:00 +0800

    优化显示

--- Main.py
+++ Main.py
@@ -9,26 +9,13 @@ import time
 
 def main():
     root = Tk()
-
-    canvas = Canvas(root, width=1260, height=260, bg = "white")
-    threading.Thread(target=collector, args=(canvas,)).start()
-
-
-
+    threading.Thread(target=collector, args=(root,)).start()
     root.mainloop()
 
 
-def collector(canvas):
-    # while True:
-    #     canvas.create_rectangle(10, 10, 110, 110, fill='red')
-    #     canvas.pack()
-    #
-    #     canvas.create_rectangle(120, 120, 210, 210, fill='blue')
-    #     canvas.pack_forget()
-    #     time.sleep(1)
-
-    serial = Serial('/dev/ttyUSB0', 57600, timeout=4)
-    cnt = 0
+def collector(root):
+    canvas = Canvas(root, width=1260, height=260, bg = "white")
+    serial = Serial('com6', 57600, timeout=4)
     count = 0
     while True:
         count = 0
@@ -42,11 +29,7 @@ def collector(canvas):
                 break
         data = serial.read(131)
         dis, df = struct.unpack('>H126s', data[0:128])
-        # print binascii.hexlify(df)
-        cnt += 1
-        # if cnt % 10 == 0:
-        #     continue
-        canvas.create_rectangle(0, 0, 1260, 260, fill='white')
+        canvas.delete("all")
         left = 0
         for i in range(0, 126):
             value = ord(df[i])
@@ -58,7 +41,11 @@ def collector(canvas):
             x2 = x1 + 8
             y2 = 255
             canvas.create_rectangle(x1, y1, x2, y2, fill='red')
+            canvas.create_text(x1 + 5, y1 - 5, text=str(i) + ':' + str(value / 3))
+        canvas.create_text(50, 50, text='dis=' + str(dis))
         canvas.pack()
+
+
 
 if __name__ == '__main__':
     main()
